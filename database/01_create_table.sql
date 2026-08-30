b77eece43ab655f1d5acd429e59355f33c303fdd
Pallavimahanor <pallavimahanor005@gmail.com>
2026-08-30 12:21:03 +0530

    Create 01_create_table.sql

---
+++ database/01_create_table.sql
@@ -0,0 +1,11 @@
+-- Employee Management System
+-- Oracle SQL Project
+-- Database Table Creation
+
+-- Department Table
+
+CREATE TABLE department (
+    department_id NUMBER PRIMARY KEY,
+    department_name VARCHAR2(50),
+    location VARCHAR2(50)
+);
