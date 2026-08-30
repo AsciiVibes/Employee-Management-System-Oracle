fe7526e1573941dc46c228ee80d6fdd863fa3f74
Pallavimahanor <pallavimahanor005@gmail.com>
2026-08-30 14:13:12 +0530

    Create database tables

---
+++ database/01_create_tables.sql
@@ -0,0 +1,33 @@
+-- =========================================
+-- Employee Management System
+-- Oracle SQL Project
+-- Table Creation
+-- =========================================
+
+
+-- Department Table
+
+CREATE TABLE department (
+    department_id NUMBER PRIMARY KEY,
+    department_name VARCHAR2(50),
+    location VARCHAR2(50)
+);
+
+
+-- Employee Table
+
+CREATE TABLE employee (
+    employee_id NUMBER PRIMARY KEY,
+    first_name VARCHAR2(50) NOT NULL,
+    last_name VARCHAR2(50),
+    email VARCHAR2(100) UNIQUE,
+    phone VARCHAR2(15),
+    hire_date DATE,
+    job_title VARCHAR2(50),
+    salary NUMBER(10,2),
+    department_id NUMBER,
+
+    CONSTRAINT fk_employee_department
+        FOREIGN KEY (department_id)
+        REFERENCES department(department_id)
+);
