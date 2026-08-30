75db54cd162513e24c141b14a59033f753d3061c
Pallavimahanor <pallavimahanor005@gmail.com>
2026-08-30 12:40:05 +0530

    Add employee table

--- database/01_create_table.sql
+++ database/01_create_table.sql
@@ -9,3 +9,24 @@ CREATE TABLE department (
     department_name VARCHAR2(50),
     location VARCHAR2(50)
 );
+
+--Employee table
+CREATE TABLE employee(
+    employee_id NUMBER PRIMARY KEY,
+    first_name VARCHAR2(50) NOT NULL,
+    last_name VARCHAR2(50),
+    email VARCHAR2(100) UNIQUE,
+    phone VARCHAR2(15),
+    hire_date DATE,
+    job_title VARCHAR2(50),
+    salary NUMBER(10,2),
+    department_id NUMBER,
+
+
+       CONSTRAINT fk_employee_department
+       FOREIGN KEY (department_id)
+       REFERENCES department(departmant_id)
+
+    );
+    
+    
